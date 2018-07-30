0951f39aca28ccb4f67bf4442e0db44eb34036e1
韩淅 <406633257@qq.com>
2018-07-30 13:27:38 +0800

    04-删除与批量删除

--- admin/categories.php
+++ admin/categories.php
@@ -37,24 +37,34 @@ function add(){
    }
 
     $imasge = '添加成功';
-   header('Location:'.$_SERVER['PHP_SELF']);
+
 
 }
 
+//TODO: 更新数据
 
 
 
-if($_SERVER['REQUEST_METHOD'] !=='GET'){
+
+
+
     if($_SERVER['REQUEST_METHOD'] ==='POST'){
         add();
     }
-}
+
+
+
+
+
 
 //TODO: 查询数据
 $query = news_bridege_sql("select * from categories");
 $categories = news_query_all($query);
 
+
+
  ?>
+
 
 
 <!DOCTYPE html>
@@ -80,24 +90,25 @@ $categories = news_query_all($query);
       </div>
       <!-- 有错误信息时展示 -->
         <?php if(isset($imasge)): ; ?>
-       <div class="alert <?php echo $imasge === '添加成功'? 'alert-success':'alert-danger'; ?>">
-        <strong><?php echo $imasge === '添加成功'? '':'错误！'; ?></strong><?php echo $imasge; ?>
+       <div class="alert <?php echo $imasge === '添加成功' || $imasge ==='更新成功'? 'alert-success':'alert-danger'; ?>">
+        <strong><?php echo $imasge === '添加成功' || $imasge === '更新成功'? '':'错误！'; ?></strong><?php echo $imasge; ?>
       </div>
         <?php endif ?>
 
       <div class="row">
-        <div class="col-md-4">
-          <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-            <h2>添加新分类目录</h2>
-            <div class="form-group">
-              <label for="name">名称</label>
-              <input id="name" class="form-control" name="name" type="text" placeholder="分类名称" <?php  echo isset($categories_name)? 'value='.$categories_name:'';?> >
-            </div>
-            <div class="form-group">
-              <label for="slug">别名</label>
-              <input id="slug" class="form-control" name="slug" type="text" placeholder="slug" <?php  echo isset($categories_slug)? 'value='.$categories_slug:'';?>>
-              <p class="help-block">https://zce.me/category/<strong>slug</strong></p>
-            </div>
+          <div class="col-md-4">
+              <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                  <h2>添加新分类目录</h2>
+                  <div class="form-group">
+                      <label for="name">名称</label>
+                      <input id="name" class="form-control" name="name" type="text" placeholder="分类名称" >
+                  </div>
+                  <div class="form-group">
+                      <label for="slug">别名</label>
+                      <input id="slug" class="form-control" name="slug" type="text" placeholder="slug" >
+                      <p class="help-block">https://zce.me/category/<strong>slug</strong></p>
+                  </div>
+
             <div class="form-group">
               <button class="btn btn-primary" type="submit">添加</button>
             </div>
@@ -106,7 +117,7 @@ $categories = news_query_all($query);
         <div class="col-md-8">
           <div class="page-action">
             <!-- show when multiple checked -->
-            <a class="btn btn-danger btn-sm" href="javascript:;" style="display: none">批量删除</a>
+            <a class="btn btn-danger btn-sm" href="/admin/api/delete.php" style="display: none">批量删除</a>
           </div>
           <table class="table table-striped table-bordered table-hover">
             <thead>
@@ -120,12 +131,12 @@ $categories = news_query_all($query);
             <tbody>
             <?php foreach ($categories as $value): ?>
               <tr>
-                <td class="text-center"><input type="checkbox"></td>
+                <td class="text-center"><input type="checkbox" data-id="<?php echo $value['id']?>"></td>
                 <td><?php echo $value['name']; ?></td>
                 <td><?php echo $value['slug']; ?></td>
                 <td class="text-center">
-                  <a href="javascript:;" class="btn btn-info btn-xs">编辑</a>
-                  <a href="javascript:;" class="btn btn-danger btn-xs">删除</a>
+                  <a href="<?php echo $_SERVER['PHP_SELF'];?>?id=<?php echo $value['id']?>" class="btn btn-info btn-xs">编辑</a>
+                  <a href="/admin/api/delete.php?id=<?php echo $value['id']?>" class="btn btn-danger btn-xs" >删除</a>
                 </td>
               </tr>
          <?php endforeach; ?>
@@ -147,47 +158,55 @@ $categories = news_query_all($query);
 
 
   $(function ($) {
-    
 
     //TODO :全选与不全选
-      var idex = 0;
-      var all_check = $('.table input:first');
-      var sons_check = $('.table tbody input');
-      var delete_btn = $('.page-action a');
 
-      all_check.on('click',function () {
+      var allCheck = $('thead input');
+      var sonsCheck = $('tbody input');
+      var delBtn = $('.page-action a');
 
-          sons_check.prop('checked',$(this).prop('checked'));
 
-          if ($(this).prop('checked')) {
-              $('.page-action a').css('display','');
+      var idx = 0;
+      var arr=[];
+      sonsCheck.on('change',function () {
+
+          var id = $(this)[0].dataset['id'];
+          var sonState= $(this).prop('checked');
+          if(sonState){
+              idx++;
+              arr.push(id);
+
           }else {
-              $('.page-action a').css('display','none');
+              idx--;
+              arr.splice(arr.indexOf(id),1);
+
           }
+          if (idx == sonsCheck.length){
+
+              allCheck.prop('checked',sonState);
+          }else{
+              allCheck.prop('checked',false);
+          }
+
+          idx == 0? delBtn.fadeOut():delBtn.fadeIn();
+          delBtn.prop('search', '?id=' + arr);
       });
 
-      sons_check.on('click',function () {
-
-          if( $(this).prop('checked')) {
-              ++idex;
-          }else{
-              --idex;
+      allCheck.on('change',function () {
+          var checking = $(this).prop('checked');
+          sonsCheck.prop('checked',checking);
+          if (checking){
+              delBtn.fadeIn();
+          } else {
+              delBtn.fadeOut();
           }
 
-          if (idex ===  sons_check.length) {
-              all_check.prop('checked',$(this).prop('checked'));
-          }else{
-              all_check.prop('checked',false);
-          }
 
-          if (idex <= sons_check.length) {
-              delete_btn.fadeIn();
-          }
-          if(idex == 0){
-              delete_btn.fadeOut();
-          }
-          console.log(idex);
       });
+
+
+
+
 
   });
 
